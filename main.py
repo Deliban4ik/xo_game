e861e0b7747d954e7fc050f20b65f198bea05bb9
Deliban4ik <76045361+Deliban4ik@users.noreply.github.com>
2022-11-24 20:10:21 +0300

    Refactored and optimized

--- main.py
+++ main.py
@@ -1,56 +1,60 @@
 def xo():
-    '''Осторожно говнокод
-    мб в будущем будет оптимизирован и станет более читабельным'''
-    team1=str(input("Choose your team\n"))
-    def check_input(slots,input, count, field):
+    """Осторожно говнокод
+    мб в будущем будет оптимизирован и станет более читабельным"""
+    team1 = str(input("Choose your team\n"))
+
+    def check_input(slot, value_of_input, count, field):
         match count:
             case 2:
-                sym="o"
-                if slots[input]==" " and input in slots.keys():
-                    slots[input]=sym
-                    print(field.format(c1=slots["1"],c2=slots["2"],c3=slots["3"],c4=slots["4"],c5=slots["5"],c6=slots["6"],c7=slots["7"],c8=slots["8"],c9=slots["9"]))
-                    if slots["1"]==slots["2"]==slots["3"]==sym:
+                sym = "o"
+                if slot[value_of_input] == " " and value_of_input in slot.keys():
+                    slot[value_of_input] = sym
+                    print(field.format(c1=slot["1"], c2=slot["2"], c3=slot["3"], c4=slot["4"], c5=slot["5"],
+                                       c6=slot["6"], c7=slot["7"], c8=slot["8"], c9=slot["9"]))
+                    if slot["1"] == slot["2"] == slot["3"] == sym:
                         print(f"o win")
-                    elif slots["4"]==slots["5"]==slots["6"]==sym:
+                    elif slot["4"] == slot["5"] == slot["6"] == sym:
                         print(f"o win")
-                    elif slots["7"]==slots["8"]==slots["9"]==sym:
+                    elif slot["7"] == slot["8"] == slot["9"] == sym:
                         print(f"o win")
-                    elif slots["1"]==slots["4"]==slots["7"]==sym:
+                    elif slot["1"] == slot["4"] == slot["7"] == sym:
                         print(f"o win")
-                    elif slots["2"]==slots["5"]==slots["8"]==sym:
+                    elif slot["2"] == slot["5"] == slot["8"] == sym:
                         print(f"o win")
-                    elif slots["3"]==slots["6"]==slots["9"]==sym:
+                    elif slot["3"] == slot["6"] == slot["9"] == sym:
                         print(f"o win")
-                    elif slots["1"]==slots["5"]==slots["9"]==sym:
+                    elif slot["1"] == slot["5"] == slot["9"] == sym:
                         print(f"o win")
-                    elif slots["7"]==slots["5"]==slots["3"]==sym:
+                    elif slot["7"] == slot["5"] == slot["3"] == sym:
                         print(f"o win")
                 else:
                     pass
             case 1:
-                sym="x"
-                if slots[input]==" " and input in slots.keys():
-                    slots[input]=sym
-                    print(field.format(c1=slots["1"],c2=slots["2"],c3=slots["3"],c4=slots["4"],c5=slots["5"],c6=slots["6"],c7=slots["7"],c8=slots["8"],c9=slots["9"]))
-                    if slots["1"]==slots["2"]==slots["3"]==sym:
+                sym = "x"
+                if slot[value_of_input] == " " and value_of_input in slot.keys():
+                    slot[value_of_input] = sym
+                    print(field.format(c1=slot["1"], c2=slot["2"], c3=slot["3"], c4=slot["4"], c5=slot["5"],
+                                       c6=slot["6"], c7=slot["7"], c8=slot["8"], c9=slot["9"]))
+                    if slot["1"] == slot["2"] == slot["3"] == sym:
                         print(f"x win")
-                    elif slots["4"]==slots["5"]==slots["6"]==sym:
+                    elif slot["4"] == slot["5"] == slot["6"] == sym:
                         print(f"x win")
-                    elif slots["7"]==slots["8"]==slots["9"]==sym:
+                    elif slot["7"] == slot["8"] == slot["9"] == sym:
                         print(f"x win")
-                    elif slots["1"]==slots["4"]==slots["7"]==sym:
+                    elif slot["1"] == slot["4"] == slot["7"] == sym:
                         print(f"x win")
-                    elif slots["2"]==slots["5"]==slots["8"]==sym:
+                    elif slot["2"] == slot["5"] == slot["8"] == sym:
                         print(f"x win")
-                    elif slots["3"]==slots["6"]==slots["9"]==sym:
+                    elif slot["3"] == slot["6"] == slot["9"] == sym:
                         print(f"x win")
-                    elif slots["1"]==slots["5"]==slots["9"]==sym:
+                    elif slot["1"] == slot["5"] == slot["9"] == sym:
                         print(f"x win")
-                    elif slots["7"]==slots["5"]==slots["3"]==sym:
+                    elif slot["7"] == slot["5"] == slot["3"] == sym:
                         print(f"x win")
                 else:
                     pass
-    slots={
+
+    slots = {
         "1": " ",
         "2": " ",
         "3": " ",
@@ -59,36 +63,36 @@ def xo():
         "6": " ",
         "7": " ",
         "8": " ",
-        "9": " ",}
-    field="|---|---|---|\n| {c1} | {c2} | {c3} |\n|---|---|---|\n| {c4} | {c5} | {c6} |\n|---|---|---|\n| {c7} | {c8} | {c9} |\n|---|---|---|"
+        "9": " ", }
+    field1 = "|---|---|---|\n| {c1} | {c2} | {c3} |\n|---|---|---|\n| {c4} | {c5} | {c6} |\n"
+    field = field1+"|---|---|---|\n| {c7} | {c8} | {c9} |\n|---|---|---|"
     while True:
-        if team1=="x":
-            x=str(input())
-            count=1
-            if x=="quit" or x=="":
+        if team1 == "x":
+            x = str(input())
+            count = 1
+            if x == "quit" or x == "":
                 break
             check_input(slots, x, count, field)
-            o=str(input())
-            count+=1
-            if o=="quit" or o=="":
+            o = str(input())
+            count += 1
+            if o == "quit" or o == "":
                 break
             check_input(slots, o, count, field)
-            count=0  
-        elif team1=="o":
-            o=str(input())
-            count=2
-            if o=="quit" or o=="":
+            count = 0
+        elif team1 == "o":
+            o = str(input())
+            count = 2
+            if o == "quit" or o == "":
                 break
             check_input(slots, o, count, field)
-            
-            x=str(input())
-            count-=1
-            if x=="quit" or x=="":
+
+            x = str(input())
+            count -= 1
+            if x == "quit" or x == "":
                 break
             check_input(slots, x, count, field)
-            count=0  
         else:
             break
-xo()
 
-    
+
+xo()
